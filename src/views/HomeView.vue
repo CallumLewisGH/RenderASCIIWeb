665ca17b20365f3d61230625fa8f1cce94ccb2e7
Callum Lewis <callumlewis852@gmail.com>
2024-11-02 14:32:41 +0000

    Initial Commit 2

---
+++ src/views/HomeView.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import Contact from '@/components/Contact.vue';
+import FileActions from '@/components/FileActions.vue';
+import ImageActions from '@/components/ImageActions.vue';
+import TextPreview from '@/components/TextPreview.vue';
+
+</script>
+
+<template>
+  <div class="ContentBox">
+    <ImageActions/>
+    <FileActions/>
+    <TextPreview/>
+  </div>
+
+  <Contact/>
+</template>
+
